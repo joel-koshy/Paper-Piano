74758ee855ec401236c8f4ab0545a57868562631
Jojo <joeljeny@gmail.com>
2025-02-08 16:55:24 -0500

    Changed to fingertips added keyboard nodes

--- src/HandLandMarker.jsx
+++ src/HandLandMarker.jsx
@@ -30,34 +30,59 @@ const Model = () => {
             }
         };
 
-    const drawLandmarks = (landmarksArray) => {
+    let prevLocation = 0; 
+    const increment = 1/14; 
+    const keyboardNodes = []
+    const y_pos = .5;
+    for (let i = 0; i < 14; i++) {
+        keyboardNodes.push({x:  i*increment, y: y_pos, data:i.toString}); 
+        prevLocation = prevLocation + increment; 
+    }
+   
+    console.log(keyboardNodes);
+
+    const drawLandmarks = (landmarksArray, keyboardNodes) => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
+    // console.log("HELLLOW!!!!", canvas.width)
     if (videoRef.current) {
                 canvas.width = videoRef.current.videoWidth;
                 canvas.height = videoRef.current.videoHeight;
     }
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'white';
-    // const fingertips = [landmarksArray[4], landmarksArray[8], landmarksArray[12], landmarksArray[16], landmarksArray[20] ];
-    landmarksArray.forEach(landmarks => {
-        landmarks.forEach(landmark => {
+    if(landmarksArray.length > 0){
+        const fingertips = [landmarksArray[0][4], landmarksArray[0][8], landmarksArray[0][12], landmarksArray[0][16], landmarksArray[0][20] ];
+        fingertips.forEach(landmark => {
             const x = landmark.x * canvas.width;
             const y = landmark.y * canvas.height;
-
             ctx.beginPath();
             ctx.arc(x, y, 5, 0, 2 * Math.PI); // Draw a circle for each landmark
             ctx.fill();
         });
-    });
-};
+    }
 
-        const detectHands = () => {
+        keyboardNodes.forEach(node => {
+            const rect_width = 300; 
+            const rect_height = 200;;
+            const x = node.x* rect_width;
+            const y = node.y* rect_height; 
+            ctx.beginPath();
+            ctx.arc(x, y, 5, 0, 2 * Math.PI); // Draw circle for each keyboard node
+            ctx.fillStyle = 'red'; // You can change the color for keyboard nodes
+            ctx.fill();
+        });
+};
+            const detectHands = () => {
             if (videoRef.current && videoRef.current.readyState >= 2) {
                 const detections = handLandmarker.detectForVideo(videoRef.current, performance.now());
                 setHandPresence(detections.handednesses.length > 0);
                 if (detections.landmarks) {
-                    drawLandmarks(detections.landmarks);
+                    // ctx.beginPath();
+                    // ctx.arc(10, 10, 10, 0, 2 * Math.PI); // Draw circle for each keyboard node
+                    // ctx.fill();
+                    drawLandmarks(detections.landmarks, keyboardNodes);
+                    // drawKeyboard(keyboardNodes); 
                 }
             }
             requestAnimationFrame(detectHands);
