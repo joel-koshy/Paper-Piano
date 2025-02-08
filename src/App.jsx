735a91942c098973d4910c636ee9761c316d9a95
Jojo <joeljeny@gmail.com>
2025-02-08 12:16:45 -0500

    intial handrecog

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Model from './HandLandMarker.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Paper Piano!!</h1>
+      <Model/>
     </>
   )
 }
